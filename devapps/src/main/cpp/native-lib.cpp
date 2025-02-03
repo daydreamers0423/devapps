9f91f9edfcce230b2b9bc324357603e145a141dc
daydreamers <daydreamers0423@gmail.com>
2025-02-03 11:02:55 +0530

    modified links for testing

--- devapps/src/main/cpp/native-lib.cpp
+++ devapps/src/main/cpp/native-lib.cpp
@@ -10,14 +10,14 @@ extern "C"
 JNIEXPORT jstring  JNICALL
 Java_in_daydreamers_devapps_UpdateWorker_getServiceUrl(JNIEnv *env, jobject thiz) {
 // Return the service URL
-return env->NewStringUTF("http://192.168.130.226:5001/devapps-446507/us-central1");
+return env->NewStringUTF("http://192.168.134.226:5001/devapps-446507/us-central1");
 }
 
 extern "C"
 JNIEXPORT jstring  JNICALL
 Java_in_daydreamers_devapps_DAnalyticsHelper_getServiceUrl(JNIEnv *env, jobject thiz) {
 // Return the service URL
-    return env->NewStringUTF("http://192.168.130.226:5001/devapps-446507/us-central1");
+    return env->NewStringUTF("http://192.168.134.226:5001/devapps-446507/us-central1");
 }
 
 extern "C"
