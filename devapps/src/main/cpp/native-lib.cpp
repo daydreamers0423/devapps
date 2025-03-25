f5ddcbcb94535b77ecc8c4650bbad8236ae38779
daydreamers <daydreamers0423@gmail.com>
2025-03-25 15:04:26 +0530

    cpp links modified to firebase function

--- devapps/src/main/cpp/native-lib.cpp
+++ devapps/src/main/cpp/native-lib.cpp
@@ -10,16 +10,16 @@ extern "C"
 JNIEXPORT jstring  JNICALL
 Java_in_daydreamers_devapps_UpdateWorker_getServiceUrl(JNIEnv *env, jobject thiz) {
 // Return the service URL
-//return env->NewStringUTF("https://us-central1-devapps-446507.cloudfunctions.net");
-    return env->NewStringUTF("http://192.168.123.59:50001/devapps-446507/us-central1");
+return env->NewStringUTF("https://us-central1-devapps-446507.cloudfunctions.net");
+    //return env->NewStringUTF("http://192.168.123.59:50001/devapps-446507/us-central1");
 }
 
 extern "C"
 JNIEXPORT jstring  JNICALL
 Java_in_daydreamers_devapps_DAnalyticsHelper_getServiceUrl(JNIEnv *env, jobject thiz) {
 // Return the service URL
-    //return env->NewStringUTF("https://us-central1-devapps-446507.cloudfunctions.net");
-    return env->NewStringUTF("http://192.168.123.59:50001/devapps-446507/us-central1");
+    return env->NewStringUTF("https://us-central1-devapps-446507.cloudfunctions.net");
+    //return env->NewStringUTF("http://192.168.123.59:50001/devapps-446507/us-central1");
 }
 
 extern "C"
