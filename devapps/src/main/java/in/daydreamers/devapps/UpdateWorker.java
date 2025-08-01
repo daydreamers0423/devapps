a1a8bb8e716b530347b71e33503fcd7614691af7
daydreamers <daydreamers0423@gmail.com>
2025-08-01 12:04:48 +0530

    implemented play integrity

--- devapps/src/main/java/in/daydreamers/devapps/UpdateWorker.java
+++ devapps/src/main/java/in/daydreamers/devapps/UpdateWorker.java
@@ -4,12 +4,20 @@ package in.daydreamers.devapps;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.SystemClock;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.play.core.integrity.IntegrityManager;
+import com.google.android.play.core.integrity.IntegrityManagerFactory;
+import com.google.android.play.core.integrity.IntegrityTokenRequest;
+import com.google.android.play.core.integrity.IntegrityTokenResponse;
 import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpHeaders;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpResponse;
@@ -23,6 +31,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 public class UpdateWorker  extends Worker {
 
@@ -46,14 +55,13 @@ public class UpdateWorker  extends Worker {
         SharedPreferences.Editor es = prefs.edit();
         if(prefs.getBoolean("dirty",false)) {
             try {
-                callCloudFunction(gson.fromJson(prefs.getString("timeline", ""), HashMap.class), Objects.requireNonNull(prefs.getLong("usage", 0L)), getServiceUrl() + CLOUD_FUNCTION_URL_LOG_ANALYTICS, prefs.getString("referer", ""));
+                requestPlayIntegrityToken(context,prefs);
+                //callCloudFunction(gson.fromJson(prefs.getString("timeline", ""), HashMap.class), Objects.requireNonNull(prefs.getLong("usage", 0L)), getServiceUrl() + CLOUD_FUNCTION_URL_LOG_ANALYTICS, prefs.getString("referer", ""));
 
-            } catch (IOException e) {
+            } catch (Exception e) {
                 es.putBoolean("lastupdated", false);
                 es.apply();
                 return Result.failure();
-            } catch (Exception e) {
-                return Result.retry();
             }
 
             es.putBoolean("lastupdated", true);
@@ -65,7 +73,39 @@ public class UpdateWorker  extends Worker {
         return Result.success();
     }
 
-    public static void callCloudFunction(@NonNull Map data,@NonNull  Long usage ,@NonNull String url,String refId) throws IOException {
+    public void requestPlayIntegrityToken(Context context,final SharedPreferences prefs) {
+        Log.i("DevApps::","requestPlayIntegrityToken");
+        final String nonce = UUID.randomUUID().toString();
+
+        IntegrityManager integrityManager = IntegrityManagerFactory.create(context);
+
+        IntegrityTokenRequest request = IntegrityTokenRequest.builder()
+                .setNonce(nonce)
+                .build();
+
+        integrityManager.requestIntegrityToken(request)
+                .addOnSuccessListener(new OnSuccessListener<IntegrityTokenResponse>() {
+                    @Override
+                    public void onSuccess(IntegrityTokenResponse response) {
+                        Log.i("DevApps::","token="+response.token());
+                        Gson gson = new Gson();
+                        try {
+                            callCloudFunction(gson.fromJson(prefs.getString("timeline",""), HashMap.class), Objects.requireNonNull(prefs.getLong("usage", 0L)), getServiceUrl() + CLOUD_FUNCTION_URL_LOG_ANALYTICS,prefs.getString("referer",""),response.token(),nonce);
+                        } catch (IOException e) {
+                            Log.e("Error3333",e.toString());
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+
+
+                    }
+                });
+    }
+
+    public static void callCloudFunction(@NonNull Map data, @NonNull  Long usage , @NonNull String url, String refId, String token, String nonce) throws IOException {
         // Create an HTTP transport
         HttpTransport transport = new NetHttpTransport();
         data.put("usage",usage/1000);
@@ -79,7 +119,9 @@ public class UpdateWorker  extends Worker {
 
         // Create the POST request
         HttpRequest request = requestFactory.buildPostRequest(new GenericUrl(url), content);
-
+        HttpHeaders headers = request.getHeaders();
+        headers.put("token",token);
+        headers.put("nounce",nonce);
         // Execute the request
         HttpResponse response = request.execute();
 
